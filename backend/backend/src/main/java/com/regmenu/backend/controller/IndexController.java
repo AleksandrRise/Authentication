e256b1d96bed4e48848f2254a28d0d3103579600
Aleksandr Ershov <aleks.usbuz@gmail.com>
2024-11-11 19:53:58 -0800

    Added a constructor for 'User' class

---
+++ backend/backend/src/main/java/com/regmenu/backend/controller/IndexController.java
@@ -0,0 +1,19 @@
+//package com.regmenu.backend.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//public class IndexController {
+//
+//    @GetMapping("/backend/../frontend/")
+//    public String index() {
+//        return "index";
+//    }
+//
+//    @PostMapping("/backend/../frontend/register")
+//    public String userRegistration() {
+//        return "index";
+//    }
+//}
