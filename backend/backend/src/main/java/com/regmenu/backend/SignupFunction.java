1eef47d5a273458464abad7211d79df380748167
Aleksandr Ershov <aleks.usbuz@gmail.com>
2024-12-18 09:29:29 -0800

    Connected frontend to backend Put thymeleaf dependency and added '.html' to the 'index' at IndexController class

--- backend/backend/src/main/java/com/regmenu/backend/SignupFunction.java
+++ backend/backend/src/main/java/com/regmenu/backend/SignupFunction.java
@@ -1,13 +1,13 @@
-package com.regmenu.backend;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-public class SignupFunction {
-
-    @PostMapping("/posting")
-    public String po(@RequestParam("name2") String username) {
-        System.out.println(username);
-        return "hello " + username;
-    }
-}
+//package com.regmenu.backend;
+//
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//public class SignupFunction {
+//
+//    @PostMapping("/register")
+//    public String po(@RequestParam("name2") String username) {
+//        System.out.println(username);
+//        return "hello " + username;
+//    }
+//}
