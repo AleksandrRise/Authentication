c1e0d2b21d20f1d59f835abd83862763bbfbb5fb
Aleksandr Ershov <aleks.usbuz@gmail.com>
2024-12-19 09:13:42 -0800

    SpringBoot App started to receive a post method from HTML

--- backend/backend/src/main/java/com/regmenu/backend/controller/IndexController.java
+++ backend/backend/src/main/java/com/regmenu/backend/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.regmenu.backend.controller;
 
+import com.regmenu.backend.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +12,12 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    @PostMapping("/register")
+    public String userRegistration(@ModelAttribute User user) {
+        System.out.println("Done!!!!");
+        return "Hello " + user;
     }
 
 }
